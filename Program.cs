d11e45eed317870ae4566b41ceb0d0d12b7654dd
ramya <ramyashankaran@yahoo.com>
2021-12-18 16:23:05 -0500

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FetchFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AWSFileDownload.Class1 awsS3Helper = new AWSFileDownload.Class1();
+
+
+            //Upload
+            string filePath = @"C:\Users\rshan\Downloads\GoogleBooksAPI.docx";
+            awsS3Helper.UploadFile(filePath);
+
+
+            awsS3Helper.GetS3Files();
+
+        }
+    }
+}
